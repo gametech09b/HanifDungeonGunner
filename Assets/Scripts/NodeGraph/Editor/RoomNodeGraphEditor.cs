abc2f4e1262cdc4b5f91647d5bbb4982c153dd75
Kentang03 <daffa03ap@gmail.com>
2023-03-10 10:03:26 +0700

    5.9_Selecting All Room Nodes

--- Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -206,8 +206,10 @@ public class RoomNodeGraphEditor : EditorWindow
     {
         GenericMenu menu = new GenericMenu();
 
-        menu.AddItem(new GUIContent("Create Room Node"), false, CreateRoomNode, mousePosition);
 
+        menu.AddItem(new GUIContent("Create Room Node"), false, CreateRoomNode, mousePosition);
+        menu.AddSeparator("");
+        menu.AddItem(new GUIContent("Select All Room Nodes"), false, SelectAllRoomNodes);
         menu.ShowAsContext();
     }
 
@@ -265,6 +267,19 @@ public class RoomNodeGraphEditor : EditorWindow
             }
         }
     }
+
+    /// <summary>
+    /// Select all room nodes
+    /// </summary>
+    private void SelectAllRoomNodes()
+    {
+        foreach (RoomNodeSO roomNode in currentRoomNodeGraph.roomNodeList)
+        {
+            roomNode.isSelected = true;
+        }
+        GUI.changed = true;
+    }
+
 
     /// <summary>
     /// Process mouse up events
