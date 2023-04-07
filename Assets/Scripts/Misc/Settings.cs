2d2a9014fad937a1e8a201ec8d296721f4dbdf50
Kentang03 <daffa03ap@gmail.com>
2023-04-07 14:17:57 +0700

    11.2_Instantiate Doors In Dungeon

--- Assets/Scripts/Misc/Settings.cs
+++ Assets/Scripts/Misc/Settings.cs
@@ -4,32 +4,37 @@ using UnityEngine;
 
 public static class Settings
 {
-  #region DUNGEON BUILD SETTINGS
-  public const int maxDungeonRebuildAttemptsForRoomGraph = 1000;
-  public const int maxDungeonBuildAttempts = 10;
-  #endregion
+    #region UNITS
+    public const float pixelsPerUnit = 16f;
+    public const float tileSizePixels = 16f;
+    #endregion
+
+    #region DUNGEON BUILD SETTINGS
+    public const int maxDungeonRebuildAttemptsForRoomGraph = 1000;
+    public const int maxDungeonBuildAttempts = 10;
+    #endregion
+
+    #region ROOM SETTINGS
+
+    public const int maxChildCorridors = 3; // Max number of child corridors leading from a room. - maximum should be 3 although this is not recommended since it can cause the dungeon building to fail since the rooms are more likely to not fit together;
+
+    #endregion
 
 
-  #region ROOM SETTINGS
+    #region ANIMATOR PARAMETERS
+    // Animator parameters - Player
+    public static int aimUp = Animator.StringToHash("aimUp");
+    public static int aimDown = Animator.StringToHash("aimDown");
+    public static int aimUpRight = Animator.StringToHash("aimUpRight");
+    public static int aimUpLeft = Animator.StringToHash("aimUpLeft");
+    public static int aimRight = Animator.StringToHash("aimRight");
+    public static int aimLeft = Animator.StringToHash("aimLeft");
+    public static int isIdle = Animator.StringToHash("isIdle");
+    public static int isMoving = Animator.StringToHash("isMoving");
+    public static int rollUp = Animator.StringToHash("rollUp");
+    public static int rollRight = Animator.StringToHash("rollRight");
+    public static int rollLeft = Animator.StringToHash("rollLeft");
+    public static int rollDown = Animator.StringToHash("rollDown");
+    #endregion
 
- // Max number of child corridors leading from a room. - maximum should be 3 although this is not recommended since it can cause the dungeon building to fail since the rooms are more likely to not fit together;
-  public const int maxChildCorridors = 3; 
-
-  #endregion
-  
-  #region ANIMATOR PARAMETERS
-  // Animator parameters - Player
-  public static int aimUp = Animator.StringToHash("aimUp");
-  public static int aimDown = Animator.StringToHash("aimDown");
-  public static int aimUpRight = Animator.StringToHash("aimUpRight");
-  public static int aimUpLeft = Animator.StringToHash("aimUpLeft");
-  public static int aimRight = Animator.StringToHash("aimRight");
-  public static int aimLeft = Animator.StringToHash("aimLeft");
-  public static int isIdle = Animator.StringToHash("isIdle");
-  public static int isMoving = Animator.StringToHash("isMoving");
-  public static int rollUp = Animator.StringToHash("rollUp");
-  public static int rollRight = Animator.StringToHash("rollRight");
-  public static int rollLeft = Animator.StringToHash("rollLeft");
-  public static int rollDown = Animator.StringToHash("rollDown");
-  #endregion
 }
